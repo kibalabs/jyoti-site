d14286720c90da4c80d5f6d6914e8b5ce6bdb8a1
Krishan Patel <krishan711@gmail.com>
2023-05-24 11:20:14 +0100

    Update events + colors

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -22,40 +22,48 @@ export const metadata = {
 export default function Press() {
   return (
     <ImageLayout imageUrl='/events.jpg' imageDescription='Jyoti Patel Events' title='Events' imageCenterPoint='30% 30%'>
-      <span>22 April 2023</span>
-      <a
-        className={`${mainFont.className} text-center font-bold text-sky-200 hover:text-sky-500 mb-8`}
-        href='https://www.roundhouse.org.uk/whats-on/2023/in-the-round-festival-2023/merky-books-literature-festival-day-1/'
-      >
-        #Merky Books Literature Festival
-      </a>
-      <span>23 April 2023</span>
-      <a
-        className={`${mainFont.className} text-center font-bold text-teal-200 hover:text-teal-500 mb-8`}
-        href='https://www.cambridgeliteraryfestival.com/events/ali-smith-debut-writers/'
-      >
-        Cambridge Literature Festival
-      </a>
       <span>26 May 2023</span>
       <a
-        className={`${mainFont.className} text-center font-bold text-yellow-200 hover:text-yellow-500 mb-8`}
+        className={`${mainFont.className} text-center font-bold text-sky-200 hover:text-sky-500 mb-8`}
         href='https://nationalcentreforwriting.org.uk/whats-on/intricate-worlds/'
         >
         Caleb Azumah Nelson & Jyoti Patel in conversation
       </a>
       <span>3 June 2023</span>
       <a
-        className={`${mainFont.className} text-center font-bold text-yellow-200 hover:text-yellow-500 mb-8`}
+        className={`${mainFont.className} text-center font-bold text-teal-200 hover:text-teal-500 mb-8`}
         href='https://www.hayfestival.com/p-20271-stormzy-five-years-of-merky-books.aspx'
       >
         Hay Festival
       </a>
-      {/* <a
-        className={`${mainFont.className} text-center font-bold text-orange-200 hover:text-orange-500 mb-8`}
-        href=''
+      <span>11 June 2023</span>
+      <a
+        className={`${mainFont.className} text-center font-bold text-yellow-200 hover:text-yellow-500 mb-8`}
+        href='https://www.bl.uk/events/jlf-jaipur-literature-festival-at-the-british-library'
       >
-        something
-      </a> */}
+        Jaipur Literary Festival at the British Library
+      </a>
+      <span>24 June 2023</span>
+      <a
+        className={`${mainFont.className} text-center font-bold text-orange-200 hover:text-orange-500 mb-8`}
+        href='https://www.glastonburyfestivals.co.uk/silver-hayes-announces-its-glastonbury-2023-line-up/'
+      >
+        Glastonbury Festival - Silver Hayes
+      </a>
+      <span>28  2023</span>
+      <a
+        className={`${mainFont.className} text-center font-bold text-sky-200 hover:text-sky-500 mb-8`}
+        href='https://www.womad.co.uk'
+      >
+        WOMAD Festival
+      </a>
+      <span>30 July 2023</span>
+      <a
+        className={`${mainFont.className} text-center font-bold text-teal-200 hover:text-teal-500 mb-8`}
+        href='https://primadonnafestival.com/year-2023/'
+      >
+        Primadonna Festival
+      </a>
     </ImageLayout>
   )
 }
